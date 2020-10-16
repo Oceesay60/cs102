7c819d7cfdc2775832340bc30215000fde11f877
Oceesay60 <71322814+Oceesay60@users.noreply.github.com>
2020-10-17 00:54:49 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,7 +1,6 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,22 +8,38 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
-
-
+    alpha_B = 'abcdefghijklmnopqrstuvwxyz'
+    ciphertext= []
+    space = 0
+    for index, ch in enumerate(text):
+        if ch != ' ':
+            mj = alpha_B.index(ch)
+            kj = alpha_B.index(key[(index - space) % len(key)])
+            cj = (mj + kj) % len(alpha_B)
+            ciphertext.append(alpha_B[cj])
+        else:
+            space += 1
+            ciphertext.append(' ')
+    return ''.join(ciphertext)
+  
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    result = []
+    space = 0
+    for index, ch in enumerate(text):
+        if ch != ' ':
+            cj = alpha_B.index(ch)
+            kj = alpha_B.index(key[(index - space) % len(key)])
+            mj = (cj - kj) % len(alpha_B)
+            result.append(alpha_B[mj])
+        else:
+            space += 1
+            result.append(' ')
+    return ''.join(result)
+  
+print(encrypt_vigenere('klic', 'tajnytext'))                 
+print(encrypt_vigenere('klic', 'tajny text s mezerama'))       
+print(encrypt_vigenere('dlouhyklic', 'mega zasifrovany text'))  
+  
+print(decrypt_vigenere('klic', 'dlrpiemzd'))                   
+print(decrypt_vigenere('klic', 'dlrpi emzd d ugjpzcwl'))        
+print(decrypt_vigenere('dlouhyklic', 'ppuu gyctntrgohf roib'))  
