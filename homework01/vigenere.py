4838b25b873229e02335ef2f06053d2bb661bf8a
Oceesay60 <71322814+Oceesay60@users.noreply.github.com>
2020-10-24 23:12:18 +0300

    Add files via upload

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,45 +1,57 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    alpha_B = 'abcdefghijklmnopqrstuvwxyz'
-    ciphertext= []
-    space = 0
-    for index, ch in enumerate(text):
-        if ch != ' ':
-            mj = alpha_B.index(ch)
-            kj = alpha_B.index(key[(index - space) % len(key)])
-            cj = (mj + kj) % len(alpha_B)
-            ciphertext.append(alpha_B[cj])
-        else:
-            space += 1
-            ciphertext.append(' ')
-    return ''.join(ciphertext)
-  
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    result = []
-    space = 0
-    for index, ch in enumerate(text):
-        if ch != ' ':
-            cj = alpha_B.index(ch)
-            kj = alpha_B.index(key[(index - space) % len(key)])
-            mj = (cj - kj) % len(alpha_B)
-            result.append(alpha_B[mj])
-        else:
-            space += 1
-            result.append(' ')
-    return ''.join(result)
-  
-print(encrypt_vigenere('klic', 'tajnytext'))                 
-print(encrypt_vigenere('klic', 'tajny text s mezerama'))       
-print(encrypt_vigenere('dlouhyklic', 'mega zasifrovany text'))  
-  
-print(decrypt_vigenere('klic', 'dlrpiemzd'))                   
-print(decrypt_vigenere('klic', 'dlrpi emzd d ugjpzcwl'))        
-print(decrypt_vigenere('dlouhyklic', 'ppuu gyctntrgohf roib'))  
+
+
+
+def encrypt_vigenere(plaintext, keyword):
+    alpha_b= "abcdefghijklmnopqrstuvwxyz "
+
+    letter_to_index = dict(zip(alpha_b, range(len(alpha_b))))
+    index_to_letter = dict(zip(range(len(alpha_b)), alpha_b))
+
+    ciphertext = ""
+    split_plaintext = [
+        plaintext[i : i + len(keyword)] for i in range(0, len(plaintext), len(keyword))
+    ]
+
+    for each_split in split_plaintext:
+        i = 0
+        for letter in each_split:
+            number = (letter_to_index[letter] + letter_to_index[keyword[i]]) % len(alpha_b)
+            ciphertext += index_to_letter[number]
+            i += 1
+
+    return ciphertext
+
+
+def decrypt(ciphertext, keyword):
+    alpha_C= "abcdefghijklmnopqrstuvwxyz "
+
+    letter_to_index = dict(zip(alpha_C, range(len(alpha_C))))
+    index_to_letter = dict(zip(range(len(alpha_C)), alpha_C))
+
+    plaintext = ""
+    split_encrypted = [
+        ciphertext[i : i + len(keyword)] for i in range(0, len(ciphertext), len(keyword))
+    ]
+
+    for each_split in split_encrypted:
+        i = 0
+        for letter in each_split:
+            number = (letter_to_index[letter] - letter_to_index[keyword[i]]) % len(alpha_C)
+            plaintext += index_to_letter[number]
+            i += 1
+
+    return plaintext
+
+
+def main():
+    plaintext = "python"
+    keyword = "banana"
+    encrypted_message = encrypt_vigenere(plaintext, keyword)
+    decrypted_message = decrypt(encrypted_message, keyword)
+
+    print("Original message: " + plaintext)
+    print("Encrypted message: " + encrypted_message)
+    print("Decrypted message: " + decrypted_message)
+
+
+main()
